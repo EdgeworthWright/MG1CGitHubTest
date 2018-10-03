edf962a4c337f0e0fed1f9e63cca27346ffe545d
EdgeworthWright <damhen80@gmail.com>
2018-10-03 18:52:45 +0200

    MG1C veranderd in MG1D

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello MG1C!");
+        System.out.println("Hello MG1D!");//C in D veranderd
 
         System.out.println("Hoe is het met jullie?");
 
